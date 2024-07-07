8614547509d2295c09fdac8a6b65333fa1ba37b8
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-07-07 23:26:19 +0100

    dd

---
+++ src/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Greeting({ name }) {
+  return (
+    <div>
+      <h2>Greeting</h2>
+      <p>Hello, {name}!</p>
+    </div>
+  );
+}
+
+export default Greeting;
